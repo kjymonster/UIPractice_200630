e3466ff8a2a7117c58533950cf6464e6f6789682
김재영 <kjymonster@naver.com>
2020-07-01 02:15:36 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/jykim3/uipractice_200630/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.jykim3.uipractice_200630
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
